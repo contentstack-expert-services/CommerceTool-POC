74436a325b7f781265c5a7b093608f5bf61b5d6c
shobhit upadhyay <shobhit.upadhyay112@gmail.com>
2025-04-01 15:38:18 +0530

    added example urls in config

--- dest.config.js
+++ dest.config.js
@@ -2,7 +2,7 @@ export const desConfig = {
     projectKey : 'Your commercetool client project key',
     clientId : 'Your commercetool client ID',
     clientSecret : 'Your commercetool client secret',
-    authUrl : 'https://auth.us-central1.gcp.commercetools.com/oauth/token',
-    apiUrl : 'https://api.us-central1.gcp.commercetools.com',
+    authUrl : 'Your commercetool auth url', // for example - https://auth.us-central1.gcp.commercetools.com/oauth/token
+    apiUrl : 'Your commercetool API url', // for example - https://api.us-central1.gcp.commercetools.com
     ExportedDataPath : 'Your Exported Data path'
 }
